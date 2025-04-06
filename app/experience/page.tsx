1501120ca2aa408930323e400525f40945bcfb62
DAzemi <Dren.azemi444@gmail.com>
2025-04-06 15:24:53 +0200

    responsive

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,5 +1,3 @@
-// app/experience/page.tsx
-
 import { FC } from "react";
 
 const Experience: FC = () => {
@@ -13,23 +11,25 @@ const Experience: FC = () => {
           <p className="text-lg sm:text-xl mt-4">Here are some of the key experiences that have helped shape my development journey.</p>
         </header>
 
-        {/* Experience Items */}
         <div className="space-y-12 pb-12">
-          <div className="bg-gray-900 p-6 rounded-lg shadow-lg">
-            <h3 className="text-2xl sm:text-3xl font-semibold">Software Engineer Intern - Pabau</h3>
-            <p className="text-gray-400 text-base sm:text-lg mt-2">November 2024 - February 2025</p>
-            <ul className="mt-4 space-y-2 text-gray-300 text-lg">
+          <div className="bg-gray-900 p-6 rounded-lg shadow-lg flex flex-col sm:flex-row justify-between items-center sm:items-start">
+            <div className="text-center sm:text-left">
+              <h3 className="text-2xl sm:text-3xl font-semibold">Software Engineer Intern - Pabau</h3>
+              <p className="text-gray-400 text-base sm:text-lg mt-2">November 2024 - February 2025</p>
+            </div>
+            <ul className="mt-4 space-y-2 text-gray-300 text-lg sm:text-base w-full sm:w-2/3">
               <li>Collaborated with a team of 60+ developers.</li>
               <li>Gained expertise in TypeScript, Next.js, GraphQL, MySQL, Hasura, CSS, and modern development practices.</li>
               <li>Worked on optimizing performance, implementing new features, and ensuring scalability.</li>
               <li>Contributed to frontend and backend improvements while maintaining clean and efficient code.</li>
             </ul>
           </div>
-
-          <div className="bg-gray-900 p-6 rounded-lg shadow-lg">
-            <h3 className="text-2xl sm:text-3xl font-semibold">Software Engineer Intern - Starlabs</h3>
-            <p className="text-gray-400 text-base sm:text-lg mt-2">September 2023 - December 2023</p>
-            <ul className="mt-4 space-y-2 text-gray-300 text-lg">
+          <div className="bg-gray-900 p-6 rounded-lg shadow-lg flex flex-col sm:flex-row justify-between items-center sm:items-start">
+            <div className="text-center sm:text-left">
+              <h3 className="text-2xl sm:text-3xl font-semibold">Software Engineer Intern - Starlabs</h3>
+              <p className="text-gray-400 text-base sm:text-lg mt-2">September 2023 - December 2023</p>
+            </div>
+            <ul className="mt-4 space-y-2 text-gray-300 text-lg sm:text-base w-full sm:w-2/3">
               <li>Developed a sophisticated e-commerce application using React.js, Node.js, and MySQL.</li>
               <li>Implemented secure authentication, authorization, and payment processing.</li>
               <li>Optimized database queries and backend performance for scalability.</li>
